1a061dc9430ca27f1ede890499ec60ffdc1fad58
George Spanos <gspanos@moby-it.com>
2023-12-08 00:46:28 +0200

    day 6 setup

---
+++ 6/main_test.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"os"
+	"testing"
+)
+
+func ReadTestFile(t *testing.T) []byte {
+	t.Helper()
+	file, err := os.ReadFile("test_input")
+	if err != nil {
+		t.Fatal("failed to read test input file")
+	}
+	return file
+}
+
+func TestPart1(t *testing.T) {
+	file := ReadTestFile(t)
+	expected := 288
+	got := part1(file)
+	if got != expected {
+		t.Fatalf("part 1 failed. expected %v. got %v", expected, got)
+	}
+}
+func TestPart2(t *testing.T) {
+	t.Fatal("not yet implemented")
+	file := ReadTestFile(t)
+	expected := 288
+	got := part2(file)
+	if got != expected {
+		t.Fatalf("part 1 failed. expected %v. got %v", expected, got)
+	}
+}
