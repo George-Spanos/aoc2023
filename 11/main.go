9275bdd5e3a837ba47a0bad2a4d4499cef86774d
George Spanos <gspanos@moby-it.com>
2023-12-21 19:57:17 +0200

    day 11 part 1

--- 11/main.go
+++ 11/main.go
@@ -3,15 +3,13 @@ package main
 import (
 	"fmt"
 	"log"
+	"math"
 	"os"
+	"strings"
 )
 
-func part1(graph []byte) int {
-	return 0
-}
-
-func part2(graph []byte) int {
-	return 0
+type Galaxy struct {
+	x, y int
 }
 
 func main() {
@@ -21,4 +19,164 @@ func main() {
 	}
 	fmt.Println("Part 1:", part1(file))
 	fmt.Println("Part 2:", part2(file))
+}
+
+func part1(file []byte) int {
+	// parse lines and save symbols
+	lines := strings.Split(strings.TrimSpace(string(file)), "\n")
+	galaxies := createGalaxies(lines)
+	sum := 0
+	pairs := createGalaxyPairs(galaxies)
+	for _, pair := range pairs {
+		distance := calcGalaxyDistance(pair[0], pair[1])
+		sum += distance
+	}
+	return sum
+}
+
+func part2(file []byte) int {
+	// parse lines and save symbols
+	lines := strings.Split(strings.TrimSpace(string(file)), "\n")
+	galaxies := createGalaxiesPart2(lines)
+	sum := 0
+	pairs := createGalaxyPairs(galaxies)
+	for _, pair := range pairs {
+		distance := calcGalaxyDistance(pair[0], pair[1])
+		sum += distance
+	}
+	return sum
+}
+func createGalaxiesPart2(lines []string) []Galaxy {
+	galaxies := make([]Galaxy, 0)
+	maxColumns := len(lines[0])
+	emptyRows := 0
+	for i, line := range lines {
+		lineGalaxies := getLineGalaxies(line, i+emptyRows)
+		if len(lineGalaxies) == 0 {
+			emptyRows += int(math.Pow10(6))
+		} else {
+			galaxies = append(galaxies, lineGalaxies...)
+		}
+	}
+	addedCols := 0
+	for j := 0; j < maxColumns+addedCols; j++ {
+		if IsEmptyColumn(galaxies, j) {
+			moveGalaxiesToRightBy(galaxies, j, int(math.Pow10(6)))
+			addedCols += int(math.Pow10(6))
+			j += int(math.Pow10(6))
+		}
+	}
+	return galaxies
+}
+func createGalaxies(lines []string) []Galaxy {
+	galaxies := make([]Galaxy, 0)
+	maxColumns := len(lines[0])
+	emptyRows := 0
+	for i, line := range lines {
+		lineGalaxies := getLineGalaxies(line, i+emptyRows)
+		if len(lineGalaxies) == 0 {
+			emptyRows++
+		} else {
+			galaxies = append(galaxies, lineGalaxies...)
+		}
+	}
+	addedCols := 0
+	for j := 0; j < maxColumns+addedCols; j++ {
+		if IsEmptyColumn(galaxies, j) {
+			moveGalaxiesToRight(galaxies, j)
+			addedCols++
+			j++
+		}
+	}
+	return galaxies
+}
+
+func getLineGalaxies(line string, lineIdx int) []Galaxy {
+	galaxies := make([]Galaxy, 0)
+	for i := range line {
+		if string(line[i]) == "#" {
+			galaxies = append(galaxies, Galaxy{x: i, y: lineIdx})
+		}
+	}
+	return galaxies
+}
+func moveGalaxiesToRight(galaxies []Galaxy, idx int) {
+	for i := range galaxies {
+		if galaxies[i].x > idx {
+			galaxies[i].x++
+		}
+	}
+}
+func moveGalaxiesToRightBy(galaxies []Galaxy, idx int, by int) {
+	for i := range galaxies {
+		if galaxies[i].x > idx {
+			galaxies[i].x += by
+		}
+	}
+}
+func createGalaxyPairs(galaxies []Galaxy) [][]Galaxy {
+	galaxyPairs := make([][]Galaxy, 0)
+	for i := range galaxies {
+		j := i + 1
+		for j < len(galaxies) {
+			galaxyPairs = append(galaxyPairs, []Galaxy{galaxies[i], galaxies[j]})
+			j++
+		}
+	}
+	return galaxyPairs
+}
+
+func calcGalaxyPairsLength(galaxies int) int {
+	return factorial(galaxies) / (factorial(2) * (factorial(galaxies - 2)))
+}
+func factorial(number int) int {
+
+	if number == 1 {
+		return 1
+	}
+
+	factorialOfNumber := number * factorial(number-1)
+
+	return factorialOfNumber
+}
+func IsEmptyColumn(galaxies []Galaxy, column int) bool {
+	for _, galaxy := range galaxies {
+		if galaxy.x == column {
+			return false
+		}
+	}
+	return true
+}
+
+func calcGalaxyDistance(galaxyA, galaxyB Galaxy) int {
+	distance := 0
+	pivot, static := getPivotX(galaxyA, galaxyB)
+	index := 0
+	for pivot+index != static {
+		index++
+	}
+	distance += index
+	index = 0
+	pivot, static = getPivotY(galaxyA, galaxyB)
+	for pivot+index != static {
+		index++
+	}
+	distance += index
+	return distance
+}
+
+func getPivotX(galaxyA, galaxyB Galaxy) (int, int) {
+	if galaxyA.x < galaxyB.x {
+		return galaxyA.x, galaxyB.x
+	} else {
+		return galaxyB.x, galaxyA.x
+	}
+}
+
+func getPivotY(galaxyA, galaxyB Galaxy) (int, int) {
+	if galaxyA.y < galaxyB.y {
+		return galaxyA.y, galaxyB.y
+	} else {
+		return galaxyB.y, galaxyA.y
+	}
 }
--- 11/main_test.go
+++ 11/main_test.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"strings"
 	"testing"
 )
 
@@ -18,10 +19,61 @@ func ReadTestFile(t *testing.T, i int) []byte {
 	}
 	return file
 }
+func TestNumberOfPairs(t *testing.T) {
+	expected := 36
+	got := calcGalaxyPairsLength(9)
+	if got != expected {
+		t.Fatalf("calcGalaxyPairsLength failed. expected %v. got %v", expected, got)
+	}
+}
+func TestFinalPositions(t *testing.T) {
+	file := ReadTestFile(t, 0)
+	lines := strings.Split(strings.TrimSpace(string(file)), "\n")
+	expected := []Galaxy{
+		{x: 4, y: 0},
+		{x: 9, y: 1},
+		{x: 0, y: 2},
+		{x: 8, y: 5},
+		{x: 1, y: 6},
+		{x: 12, y: 7},
+		{x: 9, y: 10},
+		{x: 0, y: 11},
+		{x: 5, y: 11},
+	}
+	galaxies := createGalaxies(lines)
+	for i := range expected {
+		if galaxies[i].x != expected[i].x {
+			t.Fatalf("galaxies.x not equal. expected %v. got %v", expected[i].x, galaxies[i].x)
+		}
+		if galaxies[i].y != expected[i].y {
+			t.Fatalf("galaxies.y not equal. expected %v. got %v", expected[i].y, galaxies[i].y)
+		}
+	}
+}
+
+func TestCalcDistance(t *testing.T) {
+	file := ReadTestFile(t, 0)
+	lines := strings.Split(strings.TrimSpace(string(file)), "\n")
+	expected := 15
+	galaxies := createGalaxies(lines)
+	got := calcGalaxyDistance(galaxies[0], galaxies[6])
+	if got != expected {
+		t.Fatalf("Test Calc Galaxy Distance: case 1. expected %v. got %v", expected, got)
+	}
+	expected = 17
+	got = calcGalaxyDistance(galaxies[2], galaxies[5])
+	if got != expected {
+		t.Fatalf("Test Calc Galaxy Distance: case 2. expected %v. got %v", expected, got)
+	}
+	expected = 5
+	got = calcGalaxyDistance(galaxies[7], galaxies[8])
+	if got != expected {
+		t.Fatalf("Test Calc Galaxy Distance: case 3. expected %v. got %v", expected, got)
+	}
+}
 func TestPart1(t *testing.T) {
 	file := ReadTestFile(t, 0)
-	fmt.Println(string(file))
-	expected := 4
+	expected := 374
 	got := part1(file)
 	if got != expected {
 		t.Fatalf("part 1 failed. expected %v. got %v", expected, got)
@@ -30,7 +82,7 @@ func TestPart1(t *testing.T) {
 }
 func TestPart2(t *testing.T) {
 	file := ReadTestFile(t, 2)
-	expected := 4
+	expected := 8410
 	got := part2(file)
 	if got != expected {
 		t.Fatalf("part 2 failed. expected %v. got %v", expected, got)
